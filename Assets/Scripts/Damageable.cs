1c3444a19d1e494d42e77bb08172a9ae6c0e6738
Denis Kazantsev <jokerdenkazantsev@gmail.com>
2022-07-14 00:47:48 +0300

    added projectiles hit

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    public int health;
+
+    public ParticleSystem hitEffect;
+
+    public bool invulnerable;
+    public void Hit(int damage)
+    {
+        if (invulnerable) return;
+        
+        health -= damage;
+        
+        if (health <= 0)
+        {
+            var colliders = GetComponentsInChildren<Collider>();
+            foreach (var col in colliders)
+            {
+                col.enabled = false;
+            }
+            
+            hitEffect.Play();
+            hitEffect.transform.SetParent(null);
+            // var main = hitEffect.main;
+            // main.stopAction = ParticleSystemStopAction.Destroy;
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
@@ -12,28 +14,131 @@ public class Projectile : MonoBehaviour
 
     public ParticleSystem finish;
 
+    public int damage;
+
+    private List<Collider> hitted = new List<Collider>();
+
+    /*private Rigidbody body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = transform.forward * speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (damage <= 0) return;
+        if (hitted.Contains(collision.collider)) return;
+        
+        hitted.Add(collision.collider);
+        var damageable = collision.collider.GetComponentInParent<Damageable>();
+        if (damageable != null)
+            Hit(damageable);
+
+        if (damage <= 0)
+        {
+            /*var contact = collision.GetContact(0);
+            Vector3 center = contact.point + radius * contact.normal;
+            transform.position = center;#1#
+            Finish();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (damage <= 0) return;
+        if (hitted.Contains(other)) return;
+        
+        hitted.Add(other);
+        var damageable = other.GetComponentInParent<Damageable>();
+        if (damageable != null)
+            Hit(damageable);
+
+        if (damage <= 0)
+        {
+            /*var contact = collision.GetContact(0);
+            Vector3 center = contact.point + radius * contact.normal;
+            transform.position = center;#1#
+            Finish();
+        }
+    }*/
+
     private void Update()
     {
+        // return;
         var hits = Physics.OverlapSphere(transform.position, radius, hitMask);
-        if (hits.Length > 0)
+        // Debug.Log($"overlap {hits.Length}");
+        for (int i = 0; i < hits.Length; i++)
         {
-            Finish();
-            return;
+            var hit = hits[i];
+            if (damage <= 0) return;
+            if (hitted.Contains(hit)) continue;
+            
+            hitted.Add(hit);
+            
+            var damageable = hit.GetComponentInParent<Damageable>();
+            if (damageable != null)
+                Hit(damageable);
+
+            if (damage <= 0)
+            {
+                Finish();
+                return;
+            }
         }
-        
-        
+
         var step = speed * Time.deltaTime;
         step = Mathf.Min(step, maxDistance - distance);
         var pos = transform.position + transform.forward * step;
 
-        RaycastHit hitInfo;
-        var ray = new Ray(transform.position, transform.forward);
-        if (Physics.SphereCast(ray, radius, out hitInfo, step, hitMask))
+        /*hits = Physics.OverlapCapsule(transform.position, pos, radius, hitMask);
+        // Debug.Log($"capsule {hits.Length}");
+        for (int i = 0; i < hits.Length; i++)
         {
-            pos = hitInfo.point + radius * hitInfo.normal;
-            transform.position = pos;
-            Finish();
-            return;
+            var hit = hits[i];
+            if (damage <= 0) return;
+            if (hitted.Contains(hit)) continue;
+            
+            hitted.Add(hit);
+            
+            var damageable = hit.GetComponentInParent<Damageable>();
+            if (damageable != null)
+                Hit(damageable);
+
+            if (damage <= 0)
+            {
+                pos = rayHit.point + radius * rayHit.normal;
+                transform.position = pos;
+                Finish();
+                return;
+            }
+        }*/
+        
+        var ray = new Ray(transform.position, transform.forward);
+        var rayHits = Physics.SphereCastAll(ray, radius, step, hitMask);
+        foreach (var rayHit in rayHits)
+        {
+            var hit = rayHit.collider;
+            if (hitted.Contains(hit)) continue;
+
+            hitted.Add(hit);
+            var damageable = hit.GetComponentInParent<Damageable>();
+            if (damageable != null)
+                Hit(damageable);
+            
+            if (damage <= 0)
+            {
+                pos = rayHit.point + radius * rayHit.normal;
+                transform.position = pos;
+                Finish();
+                return;
+            }
         }
             
         transform.position = pos;
@@ -42,6 +147,13 @@ public class Projectile : MonoBehaviour
         {
             Finish();
         }
+    }
+
+    private void Hit(Damageable target)
+    {
+        var hit = Mathf.Min(damage, target.health);
+        damage -= hit;
+        target.Hit(hit);
     }
 
     private void Finish()
